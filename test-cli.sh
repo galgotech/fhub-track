bd5618c7ac650fbcd7243106404b2dd74fe53e7f
André Miranda <andre@galgo.tech>
2023-01-09 08:27:14 -0300

    Fixed test-cli

--- test-cli.sh
+++ test-cli.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 BIN="go run ./cmd/fhub-track/main.go"
-#BIN="./bin/fhub-track"
 
-TEST_CLI="${BIN} -work-tree-src tmp/grafana -work-tree-dst tmp/project"
-
-# -init
-# -track public/app/core/components/NavBar
-# -status
+TEST_CLI="${BIN} --src tmp/grafana --dst tmp/project"
 
 $TEST_CLI $@
